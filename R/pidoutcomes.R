08d747eec4443cb06dd89f56db3780853702553a
gabrielfgm <gabriel.geisler@graduateinstitute.ch>
2018-02-06 14:57:25 +0200

    initial commits

---
+++ R/pidoutcomes.R
@@ -0,0 +1,7 @@
+# This is the model that
+pidoutcomes <- function(outformula, # Formula for the conditional outcome
+                        z, # name of variable indicating missing/non-missing outcomes
+                        data, # The data frame
+                        ...) {
+
+}
