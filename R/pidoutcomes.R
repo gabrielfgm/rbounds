446f2e24d49c7bfe9794f93bc164ea8807789a0e
gabrielfgm <gabriel.geisler@graduateinstitute.ch>
2018-03-16 17:32:00 +0200

    fixed weird saving error by deleting project user data, also found workaround for np bug

--- R/pidoutcomes.R
+++ R/pidoutcomes.R
@@ -3,27 +3,68 @@ dep_var_switcher <- function(form, swap) {
   reformulate(deparse(form[[3]]), response = swap)
 }
 
+# We need to fix a bug in a helper function within np, so this will replace
+#  the np function explodeFormula
+explodeFormula <- function(formula) {
+  res <- strsplit(strsplit(paste(deparse(eval(formula)), collapse=""), " *[~] *")[[1]], " *[+] *")
+  stopifnot(all(sapply(res,length) > 0))
+  names(res) <- c("response","terms")
+  res
+}
+
 
 # This is the model that estimates lower and upper bounds with missing outcomes
 pidoutcomes <- function(outformula, # Formula for the conditional outcome
                         z, # name of variable indicating missing/non-missing outcomes
                         data, # The data frame
                         ...) {
+
   # Missing data vector
   Z <- data[, z]
 
+  # now fix namespace so it reads formulas
+  tmpfun <- get("explodeFormula", envir = asNamespace("np"))
+  environment(explodeFormula) <- environment(tmpfun)
+  assignInNamespace("explodeFormula", explodeFormula, ns = "np")
+
   # estimate conditional density of outcome for known cases
-  y.model <- np::npcdens(outformula, data=data[z==1,], newdata = data)
-  y.probs_at_x <- fitted(y.model)
+  sm.data <- data[z==1,]
+  sub.bw <- np::npcdensbw(outformula,
+                          data=sm.data)
+  y.model <- np::npcdens(sub.bw, newdata = data)
+  y.probs_at_x <- y.model$condens
 
   # Estimate the conditional probability of observation
   z.model <- np::npcdens(dep_var_switcher(outformula, z), data=data, newdata=data)
-  z.probs_at_x <- fitted(z.model)
+  z.probs_at_x <- predict(z.model)
 
   # Compute worst case bounds
   m_l <- y.probs_at_x * z.probs_at_x
   m_u <- m_l + (1 - z.probs_at_x)
+
+  # Return bounds
+  list("lower" = m_l, "upper" = m_u)
 }
+
+####### Testing data
+N <- 100
+z <- rbinom(N, 1, .8)
+x_cont <- rnorm(N)
+x_dis <- rbinom(N, 1, .5)
+
+# instrument for later
+v <- rep(c(1,2,3,4), each=N/4)
+
+# errors
+e <- rnorm(N)
+
+# depvar: nonliner function
+y <- as.numeric((pnorm(x_cont) + .5 * x_dis + .2 * v + e) > 0)
+
+df <- data.frame(y, z, x_cont, x_dis, v)
+
+## Trial:
+res <- pidoutcomes(outformula =  as.factor(y) ~ x_cont + as.factor(x_dis), z ="z", data = df)
 
 # bivariate worst case bounds for binary outcomes
 wcb <- function(y, z) {
