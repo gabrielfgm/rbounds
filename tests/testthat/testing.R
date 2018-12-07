76fb61c03106a86ff87ac6b66d7e0b716893c2d3
Gabriel Mesevage <gabriel.mesevage@gmail.com>
2018-12-07 17:42:53 +0100

    fiddled with example code for testing

--- tests/testthat/testing.R
+++ tests/testthat/testing.R
@@ -11,19 +11,19 @@ fake_df <- data.frame(y, x, z)
 fake_df$yz <- fake_df$y * fake_df$z
 
 # Estimates
-np_true <- npindex(y~X1+X2+X3, data = fake_df)
-np_low <- npindex(y ~ X1+X2+X3, data = fake_df[fake_df$z==1,],
+np_true <- npreg(y ~ x, data = fake_df)
+np_low <- npreg(y ~ x, data = fake_df[fake_df$z==1,],
                 newdata = fake_df)
-np_z <- npindex(z ~ X1+X2+X3, data = fake_df)
+np_z <- npreg(z ~ x, data = fake_df)
 
 # results
 res_df <- data.frame(true = np_true$mean,
-                     low = np_low$mean * np_z$mean,
+                     low = np_low$mean*np_z$mean,
                      high = np_low$mean*np_z$mean + (1 - np_z$mean),
                      x)
 
 plot_df <- gather(res_df, model, loc_mean, true, low, high)
 
 # plotting
-plot_df %>% ggplot(aes(X1, loc_mean, group = model, color = model)) +
+plot_df %>% ggplot(aes(x, loc_mean, group = model, color = model)) +
   geom_line()
